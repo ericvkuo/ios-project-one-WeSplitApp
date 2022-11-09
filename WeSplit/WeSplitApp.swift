98e0733830591145f03a63585b6eb4006d9651d3
Eric Kuo <111590422+ericvkuo@users.noreply.github.com>
2022-11-09 15:57:53 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eric Kuo on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
